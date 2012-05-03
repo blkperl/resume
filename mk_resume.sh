7cb1fb5b3fcbd3e07fe885ead0e90b2319c4c008
Spencer Krum <nibz@cat.pdx.edu>
2012-05-02 20:19:10 -0700

    Changed address, script, gitignore

--- mk_resume.sh
+++ mk_resume.sh
@@ -5,5 +5,5 @@ pdflatex $1
 shortname=`echo $1 | sed s/tex/pdf/`
 echo "Moving $shortname to webspace"
 cp $shortname /home/$USER/solaris/public_html/
-chmod 644 /home/$USER/solaris/public_html/$shortname
+chmod a+r /home/$USER/solaris/public_html/$shortname
 echo "look at http://web.cecs.pdx.edu/~$USER/$shortname"
