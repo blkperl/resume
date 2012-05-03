9a1721f8dd5421a81686a2c31ce8a87cf0d7194c
Spencer Krum <nibz@cat.pdx.edu>
2012-05-02 20:20:32 -0700

    Shell script now does cleanup

--- mk_resume.sh
+++ mk_resume.sh
@@ -6,4 +6,8 @@ shortname=`echo $1 | sed s/tex/pdf/`
 echo "Moving $shortname to webspace"
 cp $shortname /home/$USER/solaris/public_html/
 chmod a+r /home/$USER/solaris/public_html/$shortname
+rm *.aux
+rm *.log
+rm *.pdf
+rm *.out
 echo "look at http://web.cecs.pdx.edu/~$USER/$shortname"
