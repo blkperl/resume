f5d63c442571abcf5a338185c5056dcea1febcd8
Spencer Krum <nibz@cat.pdx.edu>
2012-05-02 20:07:44 -0700

    Down to a much more reasonalbe size

--- mk_resume.sh
+++ mk_resume.sh
@@ -6,4 +6,4 @@ shortname=`echo $1 | sed s/tex/pdf/`
 echo "Moving $shortname to webspace"
 cp $shortname /home/$USER/solaris/public_html/
 chmod 644 /home/$USER/solaris/public_html/$shortname
-echo "look at web.cecs.pdx.edu/~$USER/$shortname"
+echo "look at http://web.cecs.pdx.edu/~$USER/$shortname"
